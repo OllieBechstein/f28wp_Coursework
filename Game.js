763f1974f22ff51ee42132f1e8148be6a5b8cdfd
BuildTools <unconfigured@null.spigotmc.org>
2020-11-24 03:25:46 +0000

    Removed need for canvas, and started working on game server

--- Game.js
+++ Game.js
@@ -1,25 +1,26 @@
+//Class for storing data about each player
 class Player {
-  constructor(x, y, r, vel, col){
+  //constructor takes in their x and y position, the radius of the player and the velocity of the player (default 1)
+  constructor(x, y, r, vel){
       this.x = x
       this.y = y
       this.r = r
       this.vel = vel
-      this.col = col
       this.mass = r * r * Math.PI
       this.rot = 1
       this.ang = -1
   }
 }
 
-var canvas = document.getElementById("gameCanvas")
-var ctx = canvas.getContext("2d")
-
 var food = []
 var foodRad = 12
 var foodMass = foodRad*foodRad * Math.PI
 
-var width = 1280
-var height = 720
+var worldSize = 7000
+var totalFood = 2000
+
+var width = 1920
+var height = 1080
 var first = true
 
 var offsetX = 0
@@ -31,20 +32,31 @@ var client
 var playerStartRad = 32
 
 function createWorld() {
-  for (var i = 0; i < 100000; i++) {
-    var x = random(-20000, 20000)
-    var y = random(-20000, 20000)
-    food[i] = new Food(x, y, 16, '#ff0000')
+  width = $(window).width();
+  height = $(window).height();
+  for (var i = 0; i < totalFood; i++) {
+    addFood()
+    $("body").append("<h2 class=food id=" + i.toString() + "></h2>")
+    $("#"+i.toString()).hide()
   }
-  ctx.canvas.width  = width;
-  ctx.canvas.height = height;
   createPlayer();
+
+
+}
+
+function addFood(){
+  var x = random(-worldSize, worldSize)
+  var y = random(-worldSize, worldSize)
+  food[food.length-1] = new Food(x, y, 16, '#ff0000')
 }
 
 function createPlayer(){
+  $("body").append("<h1 class=player></h1>")
   client = new Player(0,0,playerStartRad,1,'#00A500')
   players[0] = client
   client.mass = Math.PI * client.r * client.r
+  $(".player").css("margin-left", (width/2-client.r + 10).toString()+"px");
+  $(".player").css("margin-top", (height/2-client.r + 10).toString()+"px");
 }
 
 function addPlayer(){
@@ -52,45 +64,41 @@ function addPlayer(){
   players[players.length-1].mass = Math.PI * players[players.length-1].r * players[players.length-1].r
 }
 
+var foodUsed = 0
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-  
+function draw(){
+  if(client.r != Math.sqrt(client.mass / Math.PI)){
+    client.r = Math.sqrt(client.mass / Math.PI)
+    $(".player").css("width", (client.r*2).toString()+"px");
+    $(".player").css("height", (client.r*2).toString()+"px");
+    
+    $(".player").css("margin-left", (width/2- client.r + 10).toString()+"px");
+    $(".player").css("margin-top", (height/2- client.r + 10).toString()+"px");
+  }
   for (var i = 0; i < food.length; i++) {
+    var iString = i.toString()
     if (food[i].x + offsetX >= 0 - foodRad && food[i].x + offsetX <= width + foodRad){
       if (food[i].y + offsetY >= 0 - foodRad && food[i].y + offsetY <= height + foodRad){
         collision(food[i])
-
-        ctx.beginPath()
-        ctx.arc(food[i].x + offsetX, food[i].y + offsetY, foodRad, 0, 2 * Math.PI)  
-        ctx.fillStyle = food[i].col;
-        ctx.fill()
-        ctx.closePath()
+        
+        $("#"+iString).css("margin-left", (food[i].x + offsetX).toString()+"px");
+        $("#"+iString).css("margin-top", (food[i].y + offsetY).toString()+"px");
+        $("#"+iString).show()
+      } else if (food[i].y + offsetY >= 100 - foodRad && food[i].y + offsetY <= height+100 + foodRad){
+        $("#"+iString).hide()
+      }
+    } else if (food[i].x + offsetX >= 100 - foodRad && food[i].x + offsetX <= width+100 + foodRad){
+      if (food[i].y + offsetY >= 100 - foodRad && food[i].y + offsetY <= height+100 + foodRad){
+        $("#"+iString).hide()
       }
     }
 
   }
-  ctx.beginPath()
-  ctx.arc(width/2,height / 2, client.r, 0, 2 * Math.PI)
-  ctx.fillStyle = '#00A500';
-  ctx.fill()
-  ctx.closePath()
-
-  for(var i = 1; i < players.length; i++){
-    ctx.beginPath()
-    ctx.arc(players[i].x+offsetX,players[i].y+offsetY, players[i].r, 0, 2 * Math.PI)
-    ctx.fillStyle = '#00A500';
-    ctx.fill()
-    ctx.closePath()
-  }
-
 }
 
 setInterval(update, 16);
 function update(){
   keyInput()
-
-  client.r = Math.sqrt(client.mass / Math.PI)
 
   scale = playerStartRad/(client.r)
 
@@ -104,11 +112,21 @@ function update(){
     client.vel *= 0.98
   } else client.vel = 1
 
-  if(food.length < 100000) {
-    for(var i = food.length; i < 100000; i++){
-      var x = random(-20000, 20000)
-      var y = random(-20000, 20000)
-      food[food.length] = new Food(x, y, 16, '#ff0000')
+  if(food.length < totalFood) {
+    for(var i = 0; i < 100; i++){
+      if(food.length < totalFood) {
+        var x = random(-worldSize, worldSize)
+        var y = random(-worldSize, worldSize)
+        food[food.length] = new Food(x, y, 16, '#ff0000')
+      }
+    }
+  }
+
+  
+
+  if(players.length > 1){
+    for(var i = 1; i < players.length; i++){
+
     }
   }
 }
@@ -126,8 +144,6 @@ function keyInput(){
     if (event.key === ' '){
       if (client.mass > 3000){
         if(!boostToggled){
-          addPlayer()
-          alert(players.length)
           client.vel += 7
           client.mass -= client.mass/4
           boostToggled = true
@@ -150,8 +166,8 @@ function loop(timestamp) {
     first = false
   }
 
-  draw()
   update()
+  draw()
 
   window.requestAnimationFrame(loop)
 }
@@ -166,48 +182,9 @@ function collision(currentFood) {
   var dist = Math.sqrt((currentFood.x+offsetX - width/2)*(currentFood.x+offsetX - width/2) + (currentFood.y+offsetY - height/2)*(currentFood.y+offsetY - height/2))
   if(dist < client.r+foodRad){
     var i = food.indexOf(currentFood)
-    food.splice(i, 1)
+    var x = random(-worldSize, worldSize)
+    var y = random(-worldSize, worldSize)
+    food[i] = new Food(x, y, 16, '#ff0000')
     client.mass += foodMass
   }
 }
-
-// experimenting with using DOM to draw
-function generateScreen() {
-  // get the reference for the body
-  var body = document.getElementsByTagName("body")[0];
-
-  // creates a <table> element and a <tbody> element
-  var tbl = document.createElement("table");
-  var tblBody = document.createElement("tbody");
-  // creating all cells
-  for (var i = 0; i < 140; i++) {
-    // creates a table row
-    var row = document.createElement("tr");
-    //Styling
-    row.style.letterSpacing = "-4px";
-    row.style.borderSpacing = "-4px";
-    row.style.lineHeight = "0"
-    row.style.padding = "0px"
-
-    for (var j = 0; j < 240; j++) {
-      // Create a <td> element and a text node, make the text
-      // node the contents of the <td>, and put the <td> at
-      // the end of the table row
-      var cell = document.createElement("td");
-      var cellText = document.createTextNode(".");
-      cell.appendChild(cellText);
-      row.appendChild(cell);
-    }
-
-    // add the row to the end of the table body
-    tblBody.appendChild(row);
-  }
-
-  // put the <tbody> in the <table>
-  tbl.appendChild(tblBody);
-  // appends <table> into <body>
-  body.appendChild(tbl);
-  // sets the border attribute of tbl to 0;
-  tbl.setAttribute("border", "0");
-}
-
--- food.js
+++ food.js
@@ -3,6 +3,5 @@ function Food(x, y, r, col) {
   this.x = x
   this.y = y
   this.r = r
-  this.col = col
   this.vel = 0
 }
