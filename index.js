a3b88c2901fa49cd402954910bcc8c49b3379210
BuildTools <unconfigured@null.spigotmc.org>
2020-11-25 08:00:49 +0000

    Implemented Multiplayer (bugs need fixed)

--- index.js
+++ index.js
@@ -28,12 +28,12 @@ class Food {
     this.x = x
     this.y = y
     this.r = r
+    this.removed = false
   }
 }
 
 //Create an array to be used to store food objects
 var food = []
-var foodOBJs = []
 //The radius of each food object
 const foodRad = 12
 //The mass of each food object (PI*r^2)
@@ -53,6 +53,8 @@ var first = true
 //Used for positioning the camera (offsetting the x and y of the world)
 var offsetX = 0
 var offsetY = 0
+var wait = 0
+var waitTime = 10
 
 //Used for zooming the camera out as the player gets larger
 var scale = 1
@@ -67,15 +69,29 @@ const playerStartRad = 32
 //The mass the player must be greater than to be able to use the boost mechanic
 const boostThreshold = 3000
 
+/*var xhr = new XMLHttpRequest();
+  xhr.open("GET", "../food.json", false);
+  xhr.onload = ajaxCallback; 
+  xhr.send(); 
+  function ajaxCallback(event){ 
+    foodOBJs = JSON.parse(event.target.responseText)
+  }*/
+
 //Start the game for a player
 function startClient(){
   //Loop for each piece of food that should be in the game
-  for (var i = 0; i < 4000; i++) {
-    food.push(new Food(foodOBJs[i].x, foodOBJs[i].y, foodRad))
+  for (var i = 0; i < food.length; i++) {
     //Create an element in the body of the HTML code to represent this piece of food, with a unique ID
     $("body").append("<h2 class=food id=" + i.toString() + "></h2>")
     //Hide this element, using the unique ID
     $("#"+i.toString()).hide()
+  }
+  //Loop for each piece of food that should be in the game
+  for (var i = 0; i < players.length; i++) {
+    //Create an element in the body of the HTML code to represent other players, with a unique id
+    $("body").append("<h1 class=player id=" + players[i].id.toString() + "></h1>")
+    //Hide this element, using the unique ID
+    $(".player,#"+i.toString()).hide()
   }
   //Set the width and height to the width and height of the window
   width = $(window).width();
@@ -91,17 +107,8 @@ function createPlayer(){
   //Set the client to be a new player at the center of the world (0, 0) with the default radius
   client = new Player(0,0,playerStartRad, players.length)
   //Add the client to the list of players, at the end of the list
-  players[players.length] = client
-  //Use the css margins to control the position on the screen of the player (These values place the player in the center of the game viewport)
-  $(".player,#" + client.id).css("margin-left", (width/2-client.r + 10).toString()+"px");
-  $(".player,#" + client.id).css("margin-top", (height/2-client.r + 10).toString()+"px");
-}
+  players[client.id] = client
 
-//Add a player to the server on the clients side
-function addPlayer(){
-  //Push the new player to the array in the center of the world (0, 0) with the default radius
-  $("body").append("<h1 class=player id=" + players.length + "></h1>")
-  players.push(new Player(0,0,playerStartRad, players.length))
 }
 
 //Set the interval of the update function to run every 16ms (approx. 60 times per second)
@@ -109,75 +116,83 @@ setInterval(updateClient, 16);
 //Update function 
 function updateClient(){
   //If the game has just been launched (if its the first frame)
-  if(first){  
+  if(wait == waitTime){  
     //Start the client
     startClient()
     //Once this has happened it is no longer the first frame, dont run it again
-    first = false
-  }
-  //Handle key input from the client
-  keyInput()
-  //If the clients radius is not accurate (based off of its current mass)
-  if(client.r != Math.sqrt(client.mass / Math.PI)){
-    //Set it to be the correct value using a formula derived from the radius formula
-    client.r = Math.sqrt(client.mass / Math.PI)
-    //Set the new size of the player, using the new radius
-    $(".player,#" + client.id).css("width", (client.r*2).toString()+"px");
-    $(".player,#" + client.id).css("height", (client.r*2).toString()+"px");
-    //Readjust the players position to make sure it is still centered
-    $(".player,#" + client.id).css("margin-left", (width/2- client.r + 16).toString()+"px");
-    $(".player,#" + client.id).css("margin-top", (height/2- client.r + 16).toString()+"px");
-    //Adjust the viewport scale
-    scale = playerStartRad/(client.r)
-  }
-  
-  //HANDLE MOVEMENT
-  //The angle of the players movement can be derived from the direction it moves in
-  client.ang += client.dir * Math.PI / 180
-  //Update the x position using a sine graph and y position using a cosine graph (multiplying by velocity)
-  client.x += client.vel * Math.sin(client.ang)
-  client.y += client.vel * Math.cos(client.ang)
-  //Set the screens offset to be equal to the clients position so that the correct part of the level is in the viewport
-  offsetX = client.x
-  offsetY = client.y
+  } else if(wait > waitTime){
+    //Handle key input from the client
+    keyInput()
+    //If the clients radius is not accurate (based off of its current mass)
+    if(client.r != Math.sqrt(client.mass / Math.PI)){
+      //Set it to be the correct value using a formula derived from the radius formula
+      client.r = Math.sqrt(client.mass / Math.PI)
+      //Set the new size of the player, using the new radius
+      $(".player,#" + client.id).css("width", (client.r*2).toString()+"px");
+      $(".player,#" + client.id).css("height", (client.r*2).toString()+"px");
+      //Readjust the players position to make sure it is still centered
+      $(".player,#" + client.id).css("margin-left", (width/2- client.r + 16).toString()+"px");
+      $(".player,#" + client.id).css("margin-top", (height/2- client.r + 16).toString()+"px");
+      //Adjust the viewport scale
+      scale = playerStartRad/(client.r)
+    }
+    //HANDLE MOVEMENT
+    //The angle of the players movement can be derived from the direction it moves in
+    client.ang += client.dir * Math.PI / 180
+    //Update the x position using a sine graph and y position using a cosine graph (multiplying by velocity)
+    client.x += client.vel * Math.sin(client.ang)
+    client.y += client.vel * Math.cos(client.ang)
+    //Set the screens offset to be equal to the clients position so that the correct part of the level is in the viewport
+    offsetX = client.x
+    offsetY = client.y
 
-  //If the client is going above the default speed (using boost)
-  if(client.vel > client.defaultVel){
-    //Decrease the players speed by 0.02x its total
-    client.vel *= 0.98
-  //Otherwise make sure the clients velocity is set to the default (not below, as sometimes the above code will make it lower than it should be)
-  } else client.vel = client.defaultVel
-  
-  
+    //If the client is going above the default speed (using boost)
+    if(client.vel > client.defaultVel){
+      //Decrease the players speed by 0.02x its total
+      client.vel *= 0.98
+    //Otherwise make sure the clients velocity is set to the default (not below, as sometimes the above code will make it lower than it should be)
+    } else client.vel = client.defaultVel
+    
+  }
+  wait++
 }
 //Draw the game to the clients screen
 function draw(){
-  //Loop through all the food in the food array
-  for (var i = 0; i < food.length; i++) {
-    //Represent the loop index as a string (To minimize the use of the .toString() function)
-    var iString = i.toString()
-    //Check to see if the food is within the viewport of the game
-    if (food[i].x + offsetX >= 0 - foodRad && food[i].x + offsetX <= width + foodRad){
-      if (food[i].y + offsetY >= 0 - foodRad && food[i].y + offsetY <= height + foodRad){
-        //If it is, check for collision with the player
-        collision(food[i])
-        
-        //Move where its being drawn on the screen to correspond with its position in the viewport (Again using CSS's margin as the offset)
-        $("#"+iString).css("margin-left", (food[i].x + offsetX).toString()+"px");
-        $("#"+iString).css("margin-top", (food[i].y + offsetY).toString()+"px");
-        //Show the food
-        $("#"+iString).show()
-      //If the food is slightly offscreen (vertically), hide it
-      } else if (food[i].y + offsetY >= -100 - foodRad && food[i].y + offsetY <= height+100 + foodRad){
-        $("#"+iString).hide()
+  if(wait > waitTime){ 
+    //Loop through all the food in the food array
+    for (var i = 0; i < food.length; i++) {
+      //Check to see if the food is within the viewport of the game
+      if(!food[i].removed){
+        if (food[i].x + offsetX >= 0 - foodRad && food[i].x + offsetX <= width + foodRad){
+          if (food[i].y + offsetY >= 0 - foodRad && food[i].y + offsetY <= height + foodRad){
+            //If it is, check for collision with the player
+            collision(food[i])
+            //Move where its being drawn on the screen to correspond with its position in the viewport (Again using CSS's margin as the offset)
+            $("#"+i).css("margin-left", (food[i].x + offsetX).toString()+"px");
+            $("#"+i).css("margin-top", (food[i].y + offsetY).toString()+"px");
+            //Show the food
+            $("#"+i).show()
+          //If the food is slightly offscreen (vertically), hide it
+          } else if (food[i].y + offsetY >= -100 - foodRad && food[i].y + offsetY <= height+100 + foodRad){
+            $("#"+i).hide()
+          }
+        //If the food is slightly offscreen (horizontally), hide it
+        } else if (food[i].x + offsetX >= -100 - foodRad && food[i].x + offsetX <= width+100 + foodRad){
+            $("#"+i).hide()
+        }
+      } else {
+        $("#"+i).hide()
       }
-    //If the food is slightly offscreen (horizontally), hide it
-    } else if (food[i].x + offsetX >= -100 - foodRad && food[i].x + offsetX <= width+100 + foodRad){
-        $("#"+iString).hide()
     }
-  }
-  for(var i = 0; i < playerOBJs.length; i++){
-    
+    for(var i = 0; i < players.length; i++){
+      //Move where its being drawn on the screen to correspond with its position in the viewport (Again using CSS's margin as the offset)
+      $(".player,#"+i).css("margin-left", (players[i].x + offsetX).toString()+"px");
+      $(".player,#"+i).css("margin-top", (players[i].y + offsetY).toString()+"px");
+      $(".player,#"+i).css("width", (players[i].r*2).toString()+"px");
+      $(".player,#"+i).css("height", (players[i].r*2).toString()+"px");
+      //Show the player
+      $(".player,#"+i).show()
+    }
   }
 }
 
@@ -246,28 +261,19 @@ function collision(currentFood) {
     //Get the index of this piece of food in the array
     var i = food.indexOf(currentFood)
     //Set new x and y 
-    var x = random(-worldSize, worldSize)
-    var y = random(-worldSize, worldSize)
-    //Replace the current piece of food with a new piece of food
-    food[i] = new Food(x, y, foodRad)
+    socket.emit('eaten', i);
+    food[i].removed = true
     //Add the mass of this piece of food to the clients mass
     client.mass += foodMass
   }
 }
-
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "../food.json", false);
-  xhr.onload = ajaxCallback; 
-  xhr.send(); 
-  function ajaxCallback(event){ 
-    foodOBJs = JSON.parse(event.target.responseText)
-  }
+  
+const io = require('socket.io-client');
 
 //format for sent data {id: 0, x: 0, y: 0, r: 0}
 // socket.io connection setup
 const socket = io(`ws://${window.location.host}`);
-const connectedPromise = new Promise(resolve => {
+const connectPromise = new Promise(resolve => {
     socket.on('connect', () => {
         console.log('Connected to server!');
         resolve();
@@ -280,20 +286,51 @@ socket.on('player-number', num => {
 })
 
 //Set the interval of the updateServer function to run every 16ms (approx. 60 times per second)
-setInterval(updateServer, 16);
+setInterval(updateServer, 16)
 function updateServer() {
+
   //sendPosition to server
-  socket.emit('updatePlayer', {id: client.id, x: client.x, y: client.y, r: client.r});
-  //Recieve position from server
-  socket.on('updatePlayer', ({id, x, y, r}) => {
-    console.log('lolxd')
-    players[id].x = x
-    players[id].y = y
-    players[id].r = r
+  if(wait > waitTime+10){
+    socket.emit('playerData', {id: client.id, x: client.x, y: client.y, r: client.r});
+  }
+}
+
+socket.on('levelData', (foo, pla) => {
+  console.log('lolxd')
+  for(var i = 0; i < totalFood; i++){
+    food.push(new Food(foo[i].x, foo[i].y, foodRad))
+  }
+  for(var i in pla){
+    players[pla[i].id] = new Player(pla[i].x, pla[i].y, pla[i].r, pla[i].id)
+  }
+})
+
+socket.on('playerData', (dat) => {
+  var found = false
+  for(var i = 0; i < players.length; i++){
+    if(players[i].id == dat.id){
+      players[i].x = dat.x
+      players[i].y = dat.y
+      players[i].r = dat.r
+      found = true
+    }
+  }
+
+  if(!found){
+    players.push(new Player(dat.x, dat.y, dat.r, dat.id))
+    $("body").append("<h1 class=player id=" + players.length + "></h1>")
+  }
+})
+
+if(wait > waitTime){
+  socket.on('foodAdded', (dat) => {
+    food[dat.id].removed = false
+    food[dat.id].x = dat.x
+    food[dat.id].y = dat.y
   })
 }
 
-
-
-
-
+  socket.on('eaten', (dat) => {
+    food[dat].removed = true
+    console.log('eaten bruh')
+  })
